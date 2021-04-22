f5391b982567e0e01a0af3f5158635dfba90e96f
Ant1B2x <antoine.bedex@tutanota.com>
2021-04-22 22:31:21 +0200

    premier jet

--- src/linda/shm/CentralizedLinda.java
+++ src/linda/shm/CentralizedLinda.java
@@ -4,10 +4,95 @@ import linda.Callback;
 import linda.Linda;
 import linda.Tuple;
 
-/** Shared memory implementation of Linda. */
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Shared memory implementation of Linda.
+ */
 public class CentralizedLinda implements Linda {
-	
+
+    private static final Object MUTEX = new Object();
+
+    private Collection<Tuple> tuples;
+
     public CentralizedLinda() {
+        this.tuples = new HashSet<>();
+    }
+
+    @Override
+    public void write(Tuple t) {
+        synchronized (MUTEX) {
+            this.tuples.add(t);
+        }
+    }
+
+    @Override
+    public Tuple take(Tuple template) {
+        Tuple result = this.read(template);
+
+
+        this.tuples.remove(result);
+        return result;
+    }
+
+    @Override
+    public Tuple read(Tuple template) {
+        while (true) {
+            synchronized (MUTEX) {
+                for (Tuple t : this.tuples) {
+                    if (t.matches(template)) {
+                        return t;
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public Tuple tryTake(Tuple template) {
+        Tuple result = this.tryRead(template);
+
+        this.tuples.remove(result);
+        return result;
+    }
+
+    @Override
+    public Tuple tryRead(Tuple template) {
+        for (Tuple t : this.tuples)
+            if (t.matches(template))
+                return t;
+
+        return null;
+    }
+
+    @Override
+    public Collection<Tuple> takeAll(Tuple template) {
+        Collection<Tuple> result = this.readAll(template);
+
+        result.forEach(t -> this.tuples.remove(t));
+        return result;
+    }
+
+    @Override
+    public Collection<Tuple> readAll(Tuple template) {
+        Collection<Tuple> result = new HashSet<>();
+        for (Tuple t : this.tuples)
+            if (t.matches(template))
+                result.add(t);
+
+        return result;
+    }
+
+    @Override
+    public void eventRegister(eventMode mode, eventTiming timing, Tuple template, Callback callback) {
+
+    }
+
+    @Override
+    public void debug(String prefix) {
+
     }
 
     // TO BE COMPLETED
