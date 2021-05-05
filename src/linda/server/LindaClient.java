54067a790e28b117a19359e80d36c3397024377a
Ant1B2x <antoine.bedex@tutanota.com>
2021-05-05 17:28:55 +0200

    debut partie 2

--- src/linda/server/LindaClient.java
+++ src/linda/server/LindaClient.java
@@ -4,23 +4,38 @@ import linda.Callback;
 import linda.Linda;
 import linda.Tuple;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.util.Collection;
 
 /** Client part of a client/server implementation of Linda.
  * It implements the Linda interface and propagates everything to the server it is connected to.
  * */
 public class LindaClient implements Linda {
-	
+
+    private LindaServer lindaServer;
+
     /** Initializes the Linda implementation.
      *  @param serverURI the URI of the server, e.g. "rmi://localhost:4000/LindaServer" or "//localhost:4000/LindaServer".
      */
     public LindaClient(String serverURI) {
-        // TO BE COMPLETED
+        //  Connexion au serveur de noms (obtention d'un handle)
+        try {
+            this.lindaServer = (LindaServerImpl) Naming.lookup("rmi://"+serverURI+"/LindaServer");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void write(Tuple t) {
-
+        try {
+            this.lindaServer.write(t);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -62,7 +77,5 @@ public class LindaClient implements Linda {
     public void debug(String prefix) {
 
     }
-
-    // TO BE COMPLETED
 
 }
--- src/linda/server/LindaServerImpl.java
+++ src/linda/server/LindaServerImpl.java
@@ -1,15 +1,59 @@
 package linda.server;
 
+import linda.Callback;
+import linda.Linda;
+import linda.Tuple;
 import linda.shm.CentralizedLinda;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Collection;
 
 public class LindaServerImpl extends UnicastRemoteObject implements LindaServer {
 
     private CentralizedLinda linda;
 
     public LindaServerImpl() throws RemoteException {
+        this.linda = new CentralizedLinda();
     }
 
+    @Override
+    public void write(Tuple t) {
+        this.linda.write(t);
+    }
+
+    @Override
+    public Tuple take(Tuple template) {
+        return this.linda.take(template);
+    }
+
+    @Override
+    public Tuple read(Tuple template) {
+        return this.linda.read(template);
+    }
+
+    @Override
+    public Tuple tryTake(Tuple template) {
+        return this.linda.tryTake(template);
+    }
+
+    @Override
+    public Tuple tryRead(Tuple template) {
+        return this.linda.tryRead(template);
+    }
+
+    @Override
+    public Collection<Tuple> takeAll(Tuple template) {
+        return this.linda.takeAll(template);
+    }
+
+    @Override
+    public Collection<Tuple> readAll(Tuple template) {
+        return this.readAll(template);
+    }
+
+    @Override
+    public void eventRegister(Linda.eventMode mode, Linda.eventTiming timing, Tuple template, Callback callback) {
+        this.linda.eventRegister(mode, timing, template, callback);
+    }
 }
