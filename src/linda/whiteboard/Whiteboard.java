f9b73acd17a046add052334ceef7a393590e736d
Ant1B2x <antoine.bedex@tutanota.com>
2021-05-07 17:39:50 +0200

    corrected some whiteboard bugs

--- src/linda/whiteboard/Whiteboard.java
+++ src/linda/whiteboard/Whiteboard.java
@@ -1,37 +1,13 @@
 /*
 ** @author philippe.queinnec@enseeiht.fr
-** Based on IBM TSpaces exemples.
+** Inspired by IBM TSpaces exemples.
 **
 **/
 
 package linda.whiteboard;
 
-import java.awt.*;
+public class Whiteboard {
 
-/**
- ** This class implements a 'shared' whiteboard to be used with Linda.
- ** All of the Linda related code is WhiteboardPanel.java
- ** 
-*/
-public class Whiteboard extends Panel {
-
-    protected static final int WIDTH = 300;
-    protected static final int HEIGHT = 350;
-    public Frame appFrame;
-        
-    public Whiteboard(String serverURI) {
-        appFrame = new Frame("Whiteboard");
-        appFrame.add("Center", this);
-        appFrame.setSize(WIDTH,HEIGHT);   
-    
-        setLayout(new BorderLayout());
-        WhiteboardPanel wp = new WhiteboardPanel(this, new linda.server.LindaClient(serverURI));
-        add("Center", wp); 
- 
-        appFrame.setVisible(true);
-
-    }
-        
     /*** main **
      ** Run the whiteboard as an application.
      **
@@ -42,7 +18,10 @@ public class Whiteboard extends Panel {
     		System.err.println("Whiteboard serverURI.");
     		return;
     	}
-        new Whiteboard(args[0]);
+        WhiteboardModel model = new WhiteboardModel();
+        WhiteboardView view = new WhiteboardView(model);
+        model.setView(view);
+        model.start(new linda.server.LindaClient(args[0]));
     }
 }
 
--- src/linda/whiteboard/WhiteboardControls.java
+++ src/linda/whiteboard/WhiteboardControls.java
@@ -15,21 +15,31 @@ import java.awt.*;
  */
 public class WhiteboardControls extends Panel implements ItemListener {
 
-    private WhiteboardPanel target;
+    private WhiteboardView view;
+    private WhiteboardModel model;
     private static String ERASEALL_LABEL = "Erase All";
     private static String EXIT_LABEL = "Exit";
+    private static String ROTATE_LABEL = "Rotate";
+    private static String EXCLUSIVE_LABEL = "Exclusive Access";
+    private static String PENDING_LABEL = "Pending...";
+    private static String RELEASE_LABEL = "Release Exclusive";
+
+    private Button eraseAllButton;
+    private Button rotateButton;
+    private Button exitButton;
     
-    private Button eraseAllButton = null;
-    private Button exitButton = null;
+    private Button exclusiveButton;
+    private ExclusiveButtonAction exclusiveButtonAction;
     
     /**
      ** The constructor.
      **
      ** @param target - the whiteboard panel
      */
-    public WhiteboardControls(Panel _target) {
+    public WhiteboardControls(WhiteboardView view, WhiteboardModel model) {
         
-        this.target = (WhiteboardPanel)_target;
+        this.view = view;
+        this.model = model;
         Panel pLine1 = new Panel();
         Panel pLine2 = new Panel();
         setLayout(new GridLayout(2,1));
@@ -40,28 +50,29 @@ public class WhiteboardControls extends Panel implements ItemListener {
         
         eraseAllButton = new Button();
         eraseAllButton.setLabel(ERASEALL_LABEL);
-        eraseAllButton.addActionListener(
-                new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        target.eraseAll();
-                    }
-                });
+        eraseAllButton.addActionListener((e) -> { model.eraseAll(); });
         pLine1.add(eraseAllButton);
+        
+        rotateButton = new Button();
+        rotateButton.setLabel(ROTATE_LABEL);
+        rotateButton.addActionListener((e) -> { model.rotateAll(90); });
+        pLine1.add(rotateButton);
+
+        exclusiveButton = new Button();
+        exclusiveButton.setLabel(EXCLUSIVE_LABEL);
+        exclusiveButtonAction = new ExclusiveButtonAction(exclusiveButton, model);
+        exclusiveButton.addActionListener(exclusiveButtonAction);
+        pLine1.add(exclusiveButton);
         
         exitButton = new Button();
         exitButton.setLabel(EXIT_LABEL);
-        exitButton.addActionListener(
-                new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        target.exitcmd();
-                    }
-                });
+        exitButton.addActionListener((e) -> { model.terminate(); });
         pLine1.add(exitButton);
 
         pLine1.setBackground(Color.lightGray);
         pLine2.setBackground(Color.lightGray);
                 
-        target.setForeground(Color.red);
+        view.drawing.setForeground(Color.red);
         CheckboxGroup group = new CheckboxGroup();
         Checkbox b;
         pLine2.add(b = new Checkbox(null, group, false));
@@ -82,7 +93,7 @@ public class WhiteboardControls extends Panel implements ItemListener {
         pLine2.add(b = new Checkbox(null, group, true));
         b.addItemListener(this);
         b.setBackground(Color.black);
-        target.setForeground(b.getForeground());
+        view.drawing.setForeground(b.getForeground());
         
         Choice shapes = new Choice();
         shapes.addItemListener(this);
@@ -110,16 +121,43 @@ public class WhiteboardControls extends Panel implements ItemListener {
      */
     public void itemStateChanged(ItemEvent e) {
         if (e.getSource() instanceof Checkbox) {
-            target.setForeground(((Component)e.getSource()).getBackground());
+            view.drawing.setForeground(((Component)e.getSource()).getBackground());
         } else if (e.getSource() instanceof Choice) {
             String choice = (String) e.getItem();
             if (choice.equals("Lines")) {
-                target.setDrawMode(WhiteboardPanel.LINES);
+                view.setDrawMode(WhiteboardView.DrawMode.LINES);
             } else if (choice.equals("Points")) {
-                target.setDrawMode(WhiteboardPanel.POINTS);
+                view.setDrawMode(WhiteboardView.DrawMode.POINTS);
             } 
         }
     }
 
-} // WhiteboardControls
+    /**
+     ** Handling of button AcquireExclusive/Pending/ReleaseExclusive.
+     */
+    private class ExclusiveButtonAction implements ActionListener {
+
+        protected boolean hasExclusive = false;
+        private Button button;
+        private WhiteboardModel target;
+
+        public ExclusiveButtonAction(Button b, WhiteboardModel t) {
+            button = b;
+            target = t;
+        }
+
+       public void actionPerformed(ActionEvent e) {
+            if (! hasExclusive) {
+                target.acquireExclusiveAccess();
+                exclusiveButtonAction.hasExclusive = true;
+                exclusiveButton.setLabel(RELEASE_LABEL);
+            } else {
+                target.releaseExclusiveAccess();
+                exclusiveButton.setLabel(EXCLUSIVE_LABEL);
+                hasExclusive = false;
+            }
+        }
+    }
+
+}
 
