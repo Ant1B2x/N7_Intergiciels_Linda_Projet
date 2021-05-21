d6c701287a845fa5d0a47dcfbef74759e789bffb
Ant1B2x <antoine.bedex@tutanota.com>
2021-05-21 15:34:59 +0200

    début de commentaires

--- src/linda/shm/CentralizedLinda.java
+++ src/linda/shm/CentralizedLinda.java
@@ -13,28 +13,68 @@ import java.util.concurrent.CopyOnWriteArrayList;
  */
 public class CentralizedLinda implements Linda {
 
+    /**
+     * Classe interne représentant un couple template / callback
+     */
     private class Event {
+        /**
+         * Template recherché par le callback
+         */
         private final Tuple template;
+
+        /**
+         * Callback à appeler
+         */
         private final Callback callback;
 
+        /**
+         * Crée un event
+         * @param template le template de tuple à rechercher
+         * @param callback le callback à appeler quand on aura trouvé un tuple
+         */
         public Event(Tuple template, Callback callback) {
             this.template = template;
             this.callback = callback;
         }
 
+        /**
+         * Renvoie vrai quand tuple correspond au motif recherché a été trouvé
+         * @param tuple le tuple dont on cherche la correspondance
+         * @return vrai quand tuple correspond au motif recherché a été trouvé
+         */
         public boolean isMatching(Tuple tuple) {
             return tuple.matches(this.template);
         }
 
+        /**
+         * Appelle le callback
+         * @param t le tuple à envoyer au callback
+         */
         public void call(Tuple t) {
             this.callback.call(t);
         }
     }
 
+    /**
+     * L'espace de tuples
+     * Version thread-safe de l'ArrayList
+     * Gère l'accès concurrent
+     */
     private Collection<Tuple> tuples;
+
+    /**
+     * Événements de lecture
+     */
     private Collection<Event> readEvents;
+
+    /**
+     * Événements de retirage (mot inventé)
+     */
     private Collection<Event> takeEvents;
 
+    /**
+     * Crée un Linda centralisé
+     */
     public CentralizedLinda() {
         this.tuples = new CopyOnWriteArrayList<>();
         this.readEvents = new CopyOnWriteArrayList<>();
@@ -43,6 +83,7 @@ public class CentralizedLinda implements Linda {
 
     @Override
     public void write(Tuple t) {
+        // Appelle et retirer les callbacks read en priorité
         for (Event readEvent : this.readEvents) {
             if (readEvent.isMatching(t)) {
                 readEvent.call(t);
@@ -50,17 +91,24 @@ public class CentralizedLinda implements Linda {
             }
         }
 
+        // Appelle et retire au plus un callback take en attente
         for (Event takeEvent : this.takeEvents) {
             if (takeEvent.isMatching(t)) {
                 takeEvent.call(t);
                 this.takeEvents.remove(takeEvent);
-                return;
+                return; // si un take a été fait, on quitte la fonction, pas d'écriture
             }
         }
 
+        // Ajoute le tuple à l'espace partagé (pas de take n'a été fait)
         this.tuples.add(t);
     }
 
+    /**
+     * Fait la même chose que write, sans appeler les callback
+     * Utile notamment pour les serveur de backup
+     * Voir doc de write
+     */
     public void writeWithoutCalling(Tuple t) {
         for (Event readEvent : this.readEvents) {
             if (readEvent.isMatching(t)) {
@@ -148,17 +196,24 @@ public class CentralizedLinda implements Linda {
 
     @Override
     public void eventRegister(eventMode mode, eventTiming timing, Tuple template, Callback callback) {
+        // Initialise le tuple potentiellement lu ou pris à null
         Tuple tuple = null;
+
+        // Si on veut appeler le callback directement si un tuple correspond
         if (timing == eventTiming.IMMEDIATE) {
+            // Si on est en mode read, appeler tryRead
             if (mode == eventMode.READ) {
                 tuple = this.tryRead(template);
+            // Sinon (mode take), appeler tryTake
             } else if (mode == eventMode.TAKE) {
                 tuple = this.tryTake(template);
             }
         }
 
+        // Si un tuple a été lu ou pris, appeler le callback
         if (tuple != null) {
             callback.call(tuple);
+        // Si aucun tuple trouvé ou qu'on est pas en mode immediate, ajouter le callback à la liste idoine
         } else {
             if (mode == eventMode.READ) {
                 this.readEvents.add(new Event(template, callback));
@@ -168,14 +223,22 @@ public class CentralizedLinda implements Linda {
         }
     }
 
+    /**
+     * Renvoie l'ensemble des tuples de l'espace de tuples
+     * @return la liste des tuples
+     */
     public Collection<Tuple> getAllTuples() {
         Collection<Tuple> tuplesClone = new ArrayList<>();
         for (Tuple tuple : this.tuples) {
-            tuplesClone.add(tuple.deepclone());
+            tuplesClone.add(tuple);
         }
         return tuplesClone;
     }
 
+    /**
+     * Sauvegarder l'espace de tuples dans un fichier (sérialisation)
+     * @param filePath chemin d'accès du fichier
+     */
     public void save(String filePath) {
         try {
             FileOutputStream fileWriter = new FileOutputStream(filePath);
@@ -188,6 +251,10 @@ public class CentralizedLinda implements Linda {
         }
     }
 
+    /**
+     * Lire l'espace de tuples à partir d'un fichier (désérialisation)
+     * @param filePath chemin d'accès du fichier
+     */
     public void load(String filePath) {
         try {
             FileInputStream fileReader = new FileInputStream(filePath);
